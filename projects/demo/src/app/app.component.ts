3c49da29fe31500041c58164356e4c5e9d136e5f
SuperBGLazer <40876246+SuperBGLazer@users.noreply.github.com>
2022-07-27 13:59:53 -0500

    Improve demo and documentation.

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<lottie-animation-view [options]="options" [width]="250" [height]="250"></lottie-animation-view>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
